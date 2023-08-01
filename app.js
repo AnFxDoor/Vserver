635b8525b9793ed837c0e3146fd8a6b6bb7bea49
AnFxDoor <123798369+AnFxDoor@users.noreply.github.com>
2023-08-02 07:56:55 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'6a7aa9ad-af00-4e37-8c8d-9b4e78765035').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
