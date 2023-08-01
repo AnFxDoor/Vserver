a6cef22bf120ed0398c46be0c5d79a367bb753cc
AnFxDoor <123798369+AnFxDoor@users.noreply.github.com>
2023-08-02 07:57:31 +0800

    更新 app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '6a7aa9ad-af00-4e37-8c8d-9b4e78765035';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
